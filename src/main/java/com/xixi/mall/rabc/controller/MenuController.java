aac979b067cd337605e860aedd8362c79198ba64
xixichen <xixichen698@outlook.com>
2023-05-25 00:32:49 +0800

    Revert "菜单分页查询"
    This reverts commit abe9bab75958580fb51b680daf9ff576537384d4.

--- src/main/java/com/xixi/mall/rabc/controller/MenuController.java
+++ src/main/java/com/xixi/mall/rabc/controller/MenuController.java
@@ -9,14 +9,12 @@ import com.xixi.mall.rabc.vo.MenuVo;
 import com.xixi.mall.rabc.vo.RouteVo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
 import java.util.List;
 
-@Slf4j
 @RestController
 @Api(tags = "菜单接口")
 @RequestMapping(value = "/menu")
